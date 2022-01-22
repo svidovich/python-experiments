d3be7758bfdbb615c4a074cfb7ae26f0a75ad579
Samuel Vidovich <samuel.vidovich@gmail.com>
2022-01-22 11:00:08 -0500

    User specifies iteration count

--- generation_accumulation/test.py
+++ generation_accumulation/test.py
@@ -1,19 +1,21 @@
-from audioop import mul
-from math import floor
+import argparse
 import time
+from math import floor
 from random import choice
 
 from module import accumulation, generation
-
-ITERATION_COUNT = 100000
 
 ASCII_USABLE_MINIMUM = 33
 ASCII_USABLE_MAXIMUM = 126
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--iteration-count', type=int, required=False, default=100000, help='How many iterations to go through in testing')
+    args = parser.parse_args()
+    iteration_count = args.iteration_count
     list_of_strings = list()
-    for number in range(ITERATION_COUNT):
+    for _ in range(iteration_count):
         list_of_strings.append(
             chr(choice(list(range(ASCII_USABLE_MINIMUM,
                                   ASCII_USABLE_MAXIMUM)))))
@@ -31,6 +33,8 @@ def main():
         print(string)
     end = time.time()
     print(f'Printed in {end - start}s')
+
+    _ = input('Press any key to exit.')
 
 
 if __name__ == '__main__':
