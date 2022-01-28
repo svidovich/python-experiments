7eee98a87eb89545dbdc6c599c2de9b6034a6173
sam@paimon <samuel.vidovich@gmail.com>
2022-01-27 22:29:20 -0500

    Customize message sending

--- streaming_generators/scripts/send_message.py
+++ streaming_generators/scripts/send_message.py
@@ -8,26 +8,29 @@ def recv_callback(ch, method, properties, body):
 
 def main():
     parser = argparse.ArgumentParser()
-    # parser.add_argument('-u', '--queue-url', help="Where to send your message")
-    # parser.add_agument('-m', '--message', help="The message to send")
-    args = parser.parse_args()
     # yapf: disable
+    parser.add_argument('-q', '--queue-name', help='The name of the message queue')
+    parser.add_argument('-m', '--message', help='The message to send to the queue')
+    args = parser.parse_args()
+    queue_name: str = args.queue_name
+    message: str = args.message
+
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     # yapf: enable
     channel = connection.channel()
-    channel.basic_publish(exchange='', routing_key='messages', body='Yeehaw')
+    channel.basic_publish(exchange='', routing_key=queue_name, body=message)
     print('Published message.')
 
-    channel.basic_consume(queue='messages',
-                          auto_ack=True,
-                          on_message_callback=recv_callback)
+    # channel.basic_consume(queue='messages',
+    #                       auto_ack=True,
+    #                       on_message_callback=recv_callback)
 
-    print('Consuming...')
-    try:
-        channel.start_consuming()
-    except KeyboardInterrupt:
-        channel.close()
-        exit(0)
+    # print('Consuming...')
+    # try:
+    #     channel.start_consuming()
+    # except KeyboardInterrupt:
+    #     channel.close()
+    #     exit(0)
 
 
 if __name__ == '__main__':
