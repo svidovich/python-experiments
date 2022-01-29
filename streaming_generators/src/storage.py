59a01e2f35e6a99aacfc3b009ad3e90fd28cca5b
mydriasis <samuel.vidovich@gmail.com>
2022-01-28 23:34:11 -0500

    Move storage to its own file. Start definition for pg adapter. Move message adapter to its own file.

---
+++ streaming_generators/src/storage.py
@@ -0,0 +1,42 @@
+import attr
+import psycopg2
+
+from abc import ABC, abstractmethod
+
+
+@attr.s
+class PGConnectionParams(object):
+    host: str = attr.ib(kw_only=True, default='localhost')
+    dbname: str = attr.ib(kw_only=True, default='postgres')
+    username: str = attr.ib(kw_only=True)
+    password: str = attr.ib(kw_only=True)
+    port: int = attr.ib(kw_only=True, default=5432)
+
+
+class StorageAdapterBase(ABC):
+
+    @abstractmethod
+    def _connect(self):
+        raise NotImplemented()
+
+    def store_data(self, data):
+        raise NotImplemented()
+
+
+class PostgresStorageAdapter(StorageAdapterBase):
+
+    def __init__(self, connection_params: PGConnectionParams):
+        self._connect(connection_params)
+
+    def _connect(self, connection_params: PGConnectionParams) -> bool:
+        try:
+            self.connection = psycopg2.connect(
+                dbname=connection_params.dbname,
+                host=connection_params.host,
+                user=connection_params.username,
+                password=connection_params.password,
+                port=connection_params.port)
+            return True
+        except Exception as e:
+            print(f'An error occurred during connection to database: {e}')
+            return False
