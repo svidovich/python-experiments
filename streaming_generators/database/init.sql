d99d3e6813a842658859cdb4dd6342a612da2ce0
sam@paimon <samuel.vidovich@gmail.com>
2022-01-30 12:30:48 -0500

    Very basic db setup script

---
+++ streaming_generators/database/init.sql
@@ -0,0 +1,19 @@
+-- NOTE
+-- Since this is a sample project and I don't think bringing Django in is necessary,
+-- we're going to just set up the database manually. In real life, we'd probably want
+-- to use a DBA framework.
+
+CREATE USER pipeline_user;
+GRANT ALL PRIVILEGES ON DATABASE postgres TO pipeline_user;
+ALTER USER pipeline_user WITH PASSWORD 'pipeline'; -- Beware: you probably shouldn't do this.
+
+\c postgres
+
+CREATE TABLE IF NOT EXISTS test_table (
+    id SERIAL PRIMARY KEY,
+    message_id VARCHAR(64) NOT NULL,
+    message_body VARCHAR(64) NOT NULL,
+    message_timestamp INTEGER NOT NULL
+);
+
+-- ALTER TABLE test_table OWNER to spark;
