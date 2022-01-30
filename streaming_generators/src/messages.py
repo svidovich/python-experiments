c3ccc3dd2ce4ef892d9fa7c3cb9dc864d5ddbc6d
sam@paimon <samuel.vidovich@gmail.com>
2022-01-30 11:55:14 -0500

    Tons of documentation

--- streaming_generators/src/messages.py
+++ streaming_generators/src/messages.py
@@ -5,7 +5,7 @@ from utils import prime_generator
 
 from abc import ABC, abstractmethod
 from time import sleep
-from typing import Any, Generator
+from typing import Any, Generator, Tuple, Union
 
 
 @attr.s
@@ -19,20 +19,41 @@ class RabbitConnectionParams(object):
 
 
 class MessageAdapter(ABC):
-    # TODO We need some docs here
+
     @abstractmethod
-    def _connect(self):
+    def _connect(self) -> None:
+        """
+        Implementation guide:
+        Using any necessary business logic, connect to your message source.
+        Mutate class as necessary to make connection state available to other
+        instance methods.
+        """
         raise NotImplemented()
 
     @abstractmethod
-    def receive_message(self):
+    def receive_message(self) -> Any:
+        """
+        Implementation guide:
+        Using any necessary business logic, retrieve a single message from
+        your message source, and return it.
+        """
         raise NotImplemented()
 
     @abstractmethod
-    def receive_messages(self):
+    def receive_messages(self) -> Generator:
+        """
+        Implementation guide:
+        Using the receive_message method, poll the message source and yield
+        messages as they become available.
+        """
         raise NotImplemented()
 
-    def pipeline_messages(self):
+    def pipeline_messages(self, target: Generator) -> None:
+        """
+        Implementation guide:
+        Using the generator provided by receive_messages, send those messages
+        to another generator, given as the 'target' argument to this method.
+        """
         raise NotImplemented()
 
     @abstractmethod
@@ -40,11 +61,12 @@ class MessageAdapter(ABC):
         raise NotImplemented()
 
 
+# TODO Constants file?
 # Returned by the server if there was no message in the queue to retrieve;
 # see documentation: https://pika.readthedocs.io/en/stable/examples/blocking_basic_get.html
 PIKA_NULL_MESSAGE = (None, None, None)
 
-# How frequently to poll. TODO: Argument?
+# How frequently to poll.
 POLL_INTERVAL = 1.0
 
 
@@ -56,7 +78,13 @@ class RabbitMessageAdapter(MessageAdapter):
         self.channel = None
         self._connect()
 
-    def _connect(self):
+    def _connect(self) -> None:
+        """
+        Attempts to connect to a RabbitMQ queue. If credentials are provided
+        via the RabbitConnectionParams, use those; otherwise, don't use any.
+        Mutates adapter state: Sets channel and connection on instance; these
+        are used by receive_message and thus other down-streams.
+        """
         connection_params = self.connection_params
 
         # yapf: disable
@@ -80,7 +108,7 @@ class RabbitMessageAdapter(MessageAdapter):
         self.connection = pika.BlockingConnection(pika_parameters)
         self.channel = self.connection.channel()
 
-    def basic_json_deserialize(self, message):
+    def basic_json_deserialize(self, message: bytes) -> Union[bytes, dict, list]:
         """
         Takes a message received from RabbitMQ and tries to deserialize it as JSON.
         If it's successful, returns the deserialized JSON; otherwise, returns the message.
@@ -94,16 +122,25 @@ class RabbitMessageAdapter(MessageAdapter):
         """
         Sends a message to a RabbitMQ Queue.
         """
-        self.channel.basic_publish(
-            exchange=self.connection_params.exchange,
-            routing_key=self.connection_params.queue_name,
-            body=message)
+        self.channel.basic_publish(exchange=self.connection_params.exchange,
+                                   routing_key=self.connection_params.queue_name,
+                                   body=message)
 
-    def receive_message(self):
-        return self.channel.basic_get(queue=self.connection_params.queue_name,
-                                      auto_ack=True)
+    def receive_message(self) -> Tuple:
+        """
+        Retrieves a message from a RabbitMQ queue. Successful or not, returns what comes
+        back from the queue. In the case that there's nothing to retrieve, returns a tuple
+        like (None, None, None).
+        """
+        return self.channel.basic_get(queue=self.connection_params.queue_name, auto_ack=True)
 
-    def receive_messages(self) -> Generator:
+    def receive_messages(self, poll_interval: float = POLL_INTERVAL) -> Generator:
+        """
+        A polling loop that yields messages from a RabbitMQ queue.
+        TODOs:
+            - Add error handling around status from message; ideally I don't
+              think pipeline_messages needs to think about that.
+        """
         while True:
             message = self.receive_message()
             if message == PIKA_NULL_MESSAGE:
@@ -112,8 +149,11 @@ class RabbitMessageAdapter(MessageAdapter):
 
             yield message
 
-    def pipeline_messages(self, target: Generator):
+    def pipeline_messages(self, target: Generator) -> None:
         for message in self.receive_messages():
+            # TODO / NOTE, FUTURE: We can probably act on the status & properties
+            # so that we can refuse to send if the status was bad, etc. We should
+            # probably do that lower, in receive_messages or receive_message.
             status, properties, message_content = message
             message_content = self.basic_json_deserialize(message_content)
             prime_generator(target)
