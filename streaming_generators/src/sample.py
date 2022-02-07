63958f87d09bb67b0cd78b62701ab88c9d8e346d
sam@paimon <samuel.vidovich@gmail.com>
2022-02-06 20:28:34 -0500

    First swing at broadcasting

--- streaming_generators/src/sample.py
+++ streaming_generators/src/sample.py
@@ -3,15 +3,21 @@ import time
 
 from plugin import SimplePlugin
 from messages import RabbitMessageAdapter, RabbitConnectionParams
-from storage import PostgresStorageAdapter, PGConnectionParams
+from storage import ESConnectionParams, ElasticSearchStorageAdapter, PostgresStorageAdapter, PGConnectionParams
+from utils import broadcast
 
 DATABASE = os.environ["POSTGRES_DB"]
 # TODO: Things that aren't secure for 1000, Alex.
 DATABASE_USER = os.environ['PIPELINE_POSTGRES_USER']
 DATABASE_PASSWORD = os.environ['PIPELINE_POSTGRES_PASSWORD']
 DATABASE_HOST = os.environ['DATABASE_HOST']
+# ---
 MESSAGES_QUEUE = os.environ['MESSAGES_QUEUE']
 RABBITMQ_HOST = os.environ['RABBITMQ_HOST']
+# ---
+ELASTIC_HOSTNAME = os.environ['ELASTIC_HOSTNAME']
+ELASTIC_PASSWORD = os.environ['ELASTIC_PASSWORD']
+ELASTIC_USERNAME = os.environ['ELASTIC_USERNAME']
 
 
 def message_printer():
@@ -30,7 +36,7 @@ def main():
     # TODO: This is a hack to essentially let me await rabbit being up.
     # There's a cool way to do this with docker-compose itself, but we
     # can think about that later.
-    connected = False
+    db_connected = False
     while not connected:
         try:
             message_adapter = RabbitMessageAdapter(rabbit_connection_params)
@@ -43,9 +49,20 @@ def main():
                                                     dbname=DATABASE,
                                                     username=DATABASE_USER,
                                                     password=DATABASE_PASSWORD)
-    storage_adapter = PostgresStorageAdapter(postgres_connection_params)
+    pg_storage_adapter = PostgresStorageAdapter(postgres_connection_params)
 
-    message_adapter.pipeline_messages(test_plugin.processing_loop(target=storage_adapter.stream_data_to_store()))
+    elasticsearch_connection_params = ESConnectionParams(host=ELASTIC_HOSTNAME,
+                                                         username=ELASTIC_USERNAME,
+                                                         password=ELASTIC_PASSWORD)
+    es_storage_adapter = ElasticSearchStorageAdapter(elasticsearch_connection_params)
+
+    # This explicitly pulls the generators so that we can cleanly broadcast to them below
+    storage_adapter_streaming_loops = [
+        pg_storage_adapter.stream_data_to_store(),
+        es_storage_adapter.stream_data_to_store(),
+    ]
+
+    message_adapter.pipeline_messages(test_plugin.processing_loop(target=broadcast(storage_adapter_streaming_loops)))
 
 
 if __name__ == '__main__':
