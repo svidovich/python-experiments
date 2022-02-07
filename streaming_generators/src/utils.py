64210201b316db2a379b9387611f33825393af45
sam@paimon <samuel.vidovich@gmail.com>
2022-02-06 20:20:31 -0500

    Add broadcaster

--- streaming_generators/src/utils.py
+++ streaming_generators/src/utils.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Generator
+from typing import Iterator, Generator
 
 # yapf: disable
 generator_just_created = lambda g: inspect.getgeneratorstate(g) == inspect.GEN_CREATED
@@ -9,3 +9,14 @@ generator_just_created = lambda g: inspect.getgeneratorstate(g) == inspect.GEN_C
 def prime_generator(target: Generator):
     if generator_just_created(target):
         target.send(None)
+
+
+def broadcast(targets: Iterator[Generator]):
+    """
+    Send data to multiple target generators.
+    """
+    while True:
+        data = (yield)
+        for target in targets:
+            prime_generator(target)
+            target.send(data)
