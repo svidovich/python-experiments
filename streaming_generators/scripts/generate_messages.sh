23a6d001ec0acf474d3aa101ffea0ba88ce6d616
sam@paimon <samuel.vidovich@gmail.com>
2022-02-08 23:05:00 -0500

    Delete file if it's there

--- streaming_generators/scripts/generate_messages.sh
+++ streaming_generators/scripts/generate_messages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rm messages
-for i in {1..1000}; do echo '{"message_body": "'$(uuidgen)-${i}'"}' >> messages; done
+[ -f messages ] && rm messages
+for i in {1..10000}; do echo '{"message_body": "'$(uuidgen)-${i}'"}' >> messages; done
