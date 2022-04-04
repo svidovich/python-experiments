048cac4b845c57018fc8d7db3c6270abfcf5ec57
sam@paimon <samuel.vidovich@gmail.com>
2022-04-04 00:18:37 -0400

    Commit to unit test. We can't use frozen; parsing the raw bytes into our Response object requires that we transform strings to integers in some cases; this can be done in a __post_init__ method if and only if we don't use a frozen dataclass.

--- something_about_sockets/test.py
+++ something_about_sockets/test.py
@@ -7,8 +7,23 @@ class HTTPTestCase(unittest.TestCase):
     def test_parse_response_bytes(self):
         sample_response_bytes = b'HTTP/1.1 200 OK\r\nServer: Werkzeug/2.1.1 Python/3.10.4\r\nDate: Mon, 04 Apr 2022 03:00:02 GMT\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: 17\r\n\r\n{"sample":"data"}'
         output_response: Response = parse_response_bytes(sample_response_bytes)
+        output_dictionary = output_response.asdict()
+        expected_output = {
+            'body': '{"sample":"data"}',
+            'connection': None,
+            'content_length': 17,
+            'content_type': 'text/html; charset=utf-8',
+            'date': 'Mon, 04 Apr 2022 03:00:02 GMT',
+            'http_version': 'HTTP/1.1',
+            'server': 'Werkzeug/2.1.1 Python/3.10.4',
+            'status_code': 200
+        }
+        for key, value in expected_output.items():
+            self.assertEqual(
+                output_dictionary[key],
+                value
+            )
 
-        print(output_response.asdict())
 
 
 if __name__ == '__main__':
--- something_about_sockets/utils.py
+++ something_about_sockets/utils.py
@@ -2,24 +2,28 @@ import typing as T
 from dataclasses import asdict, dataclass
 
 
-@dataclass(frozen=True, kw_only=True)
+@dataclass(kw_only=True)
 class Response:
     body: str = None
     connection: str = None
     content_length: int = 0
     content_type: str = None
     date: str = None
-    header: str = None
     http_version: str = None
     server: str = None
     status_code: T.Optional[int] = None
 
     def asdict(self):
         return asdict(self)
+    
+    def __post_init__(self):
+        self.status_code = int(self.status_code) if self.status_code else None
+        self.content_length = int(self.content_length) if self.content_length else 0
+
 
 HTTP_PREFIX_MAP = {
     'Connection: ': 'connection',
-    'Content-Length': 'content_length',
+    'Content-Length: ': 'content_length',
     'Content-Type: ': 'content_type',
     'Date: ': 'date',
     'Server: ': 'server',
@@ -34,7 +38,7 @@ def parse_response_bytes(response_bytes: bytes) -> Response:
             if entry.startswith('HTTP/'):
                 status_information = entry.split(' ')
                 response_dictionary['http_version'] = status_information[0]
-                response_dictionary['status_code'] = int(status_information[1])
+                response_dictionary['status_code'] = status_information[1]
             
             else:
                 for prefix, key in HTTP_PREFIX_MAP.items():
