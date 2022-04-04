558af7065bc4711ff30360b5550173cd79e65271
sam@paimon <samuel.vidovich@gmail.com>
2022-04-04 00:08:41 -0400

    Dynamic, position-independent response parsing

--- something_about_sockets/utils.py
+++ something_about_sockets/utils.py
@@ -4,36 +4,41 @@ from dataclasses import asdict, dataclass
 
 @dataclass(frozen=True, kw_only=True)
 class Response:
-    protocol: str = None
+    http_version: str = None
     status_code: T.Optional[int] = None
-    user_agent: str = None
-    response_date: str = None
+    server: str = None
+    connection: str = None
+    content_type: str = None
+    date: str = None
     header: str = None
-    response_content_length: int = 0
+    content_length: int = 0
     body: str = None
 
     def asdict(self):
         return asdict(self)
 
+HTTP_PREFIX_MAP = {
+    'Server: ': 'server',
+    'Date: ': 'date',
+    'Content-Type: ': 'content_type',
+    'Content-Length': 'content_length',
+    'Connection: ': 'connection'
+}
 
 def parse_response_bytes(response_bytes: bytes) -> Response:
     split_bytes = response_bytes.decode('utf-8').split('\r\n')
     response_dictionary = dict()
+    response_dictionary['body'] = split_bytes.pop()
     if len(split_bytes):
+        for entry in split_bytes:
+            if entry.startswith('HTTP/'):
+                status_information = entry.split(' ')
+                response_dictionary['http_version'] = status_information[0]
+                response_dictionary['status_code'] = int(status_information[1])
+            
+            else:
+                for prefix, key in HTTP_PREFIX_MAP.items():
+                    if entry.startswith(prefix):
+                        response_dictionary[key] = entry.removeprefix(prefix)
 
-        response_info: str = split_bytes[0]
-        response_info_split = response_info.split(' ')
-
-        response_content_length = int(0)
-        try:
-            response_content_length: int = int(split_bytes[4].split(' ')[1])
-        except Exception:
-            pass
-
-        return Response(protocol=response_info_split[0] or None,
-                        status_code=int(response_info_split[1]),
-                        user_agent=split_bytes[1] or None,
-                        response_date=split_bytes[2] or None,
-                        header=split_bytes[3],
-                        response_content_length=response_content_length,
-                        body=split_bytes[6])
+        return Response(**response_dictionary)
