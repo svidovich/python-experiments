51e1674f1aba65933adbd3265b48a408ade3a1e1
sam@paimon <samuel.vidovich@gmail.com>
2022-04-03 23:13:53 -0400

    Parsing a response into a dictionary

---
+++ something_about_sockets/test.py
@@ -0,0 +1,13 @@
+from utils import parse_response_bytes
+import unittest
+
+
+class HTTPTestCase(unittest.TestCase):
+
+    def test_parse_response_bytes(self):
+        sample_response_bytes = b'HTTP/1.1 200 OK\r\nServer: Werkzeug/2.1.1 Python/3.10.4\r\nDate: Mon, 04 Apr 2022 03:00:02 GMT\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: 17\r\n\r\n{"sample":"data"}'
+        parse_response_bytes(sample_response_bytes)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
