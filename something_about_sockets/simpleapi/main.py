a277ab130cfa9287dfa085524103adc52814e7c6
sam@paimon <samuel.vidovich@gmail.com>
2022-04-05 21:57:32 -0400

    New, souped up endpoint

--- something_about_sockets/simpleapi/main.py
+++ something_about_sockets/simpleapi/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, Response
+import uuid
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
@@ -12,6 +13,23 @@ def healthcheck():
 def sample():
     return Response('{"sample":"data"}', status=200)
 
+ephemeral_store = set()
+
+@app.route("/idgen", methods=["GET", "POST", "PUT"])
+def id_endpoint():
+    id = request.args.get('id', None)
+    if request.method == "GET":
+        if id in ephemeral_store:
+            return Response(f"ID {id} found!\n", status=200)
+        else:
+            return Response(f"ID {id} not found!\n", status=404)
+    elif request.method == "POST":
+        return Response(f'{uuid.uuid4()}', status=200)
+    elif request.method == "PUT":
+        ephemeral_store.add(id)
+        return Response("OK", status=200)
+    else:
+        return Response("Not Implemented", status=501)
 
 def main():
     app.run(host='0.0.0.0', port=5000)
