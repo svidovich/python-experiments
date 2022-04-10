a886521445451478b44b11ff5a64c4d9ee7ef00a
sam@paimon <samuel.vidovich@gmail.com>
2022-04-09 22:19:14 -0400

    Send seems to work; need to receive?

--- something_about_sockets/socketexperiment.py
+++ something_about_sockets/socketexperiment.py
@@ -13,6 +13,7 @@ def args_from_env() -> dict:
         'API_PORT': os.environ.get("API_PORT"),
     }
 
+REQUEST_UA = "~socket experiment machine~"
 RECV_SIZE = 1024
 MAX_RECV_SIZE = 1024 * 1024 * 8
 SOCK_CONNECTION_TIMEOUT = float(10)
@@ -77,12 +78,44 @@ def get(inet_socket: socket, path: str) -> bytes:
 
         return response_bytes
 
+def post(inet_socket: socket, path: str, content: bytes, content_type: str = None) -> bytes:
+    request_bytes: str = f"POST {path} HTTP/1.1\r\n"
+    request_bytes += f"User-Agent: {REQUEST_UA}\r\n"
+    request_bytes += f"Content-Length: {len(content)}\r\n"
+    if content_type:
+        request_bytes += f"Content-Type: {content_type}\r\n"
+    request_bytes: bytes = request_bytes.encode()
+    request_bytes += content
+    response_bytes = bytes()
+
+    _, write_sockets, _ = select(list(), [inet_socket], list(), SOCK_CONNECTION_TIMEOUT)
+
+    if write_sockets:
+        logger.debug('Making request...')
+        inet_socket.send(request_bytes)
+
+        inet_socket.setblocking(False)
+        read_sockets, _, _ = select([inet_socket], list(), list(), SOCK_CONNECTION_TIMEOUT)
+        while True:
+            if read_sockets:
+                with Timer("Receiving bytes from socket"):
+                    response_bytes += inet_socket.recv(RECV_SIZE)
+                logger.debug(f"Response bytes increases size to {len(response_bytes)}")
+                logger.debug(f"Response bytes: {response_bytes}")
+                read_sockets: T.List[T.Optional[socket]]
+                with Timer("Checking for additional data from endpoint"):
+                    read_sockets, _, _ = select([inet_socket], list(), list(), SOCKET_RECHECK_TIMEOUT)
+            else:
+                break
+
+        return response_bytes
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--host', required=False, help='Host to hit with socket connection. Alternatively, set the API_HOST environment variable.')
     parser.add_argument('-p', '--port', required=False, type=int, help='Port on host. Alternatively, set the API_PORT environment variable.')
     parser.add_argument('-e', '--endpoint', required=False, default='/', help='The endpoint at the host to hit. Defaults to /.')
+    parser.add_argument('-x', '--method', required=False, default='GET', choices=['GET', 'POST'], help='HTTP Method to use')
     args = parser.parse_args()
 
     api_host = args.host
@@ -99,7 +132,11 @@ def main():
 
     api_path = args.endpoint
     inet_socket: socket = socket_connect(generate_socket(), api_host, api_port)
-    response: bytes = get(inet_socket, api_path)
+    if args.method == 'GET':
+        response: bytes = get(inet_socket, api_path)
+    if args.method == 'POST':
+        sample_post_content = bytes()
+        response: bytes = post(inet_socket, api_path, sample_post_content)
     socket_disconnect(inet_socket=inet_socket)
     logger.debug(response)
 
