4e2e25261f398d68c760bba72fc0ba9080344d46
sam@paimon <samuel.vidovich@gmail.com>
2022-05-02 23:03:54 -0500

    Prettier printing

--- word-frequency/main.py
+++ word-frequency/main.py
@@ -55,10 +55,15 @@ def count_words(page_text: str) -> Counter:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', required=True, type=str, help='The URL to eyeball')
+    parser.add_argument('-t', '--top', required=False, type=int, default=10, help='The top n words to show, e.g. setting 5 will return the top 5 words on the page.')
     args = parser.parse_args()
     page_text = get_page_body(args.url)
     counts = count_words(page_text)
-    print(counts.most_common(20))
+    for index, word_tuple in enumerate(counts.most_common(args.top)):
+        word: str
+        count: int
+        word, count = word_tuple
+        print(f'{index + 1}. {word}: {count}')
 
 if __name__ == '__main__':
     main()
