bcc68edc1a6c14786b9ecfdcb85d1bcf40aff35d
sam@paimon <samuel.vidovich@gmail.com>
2022-08-14 13:41:39 -0400

    I can move around, albeit with some tearing

--- pygame-something/sample.py
+++ pygame-something/sample.py
@@ -4,7 +4,9 @@ from typing import Final, Tuple
 
 import pygame
 from pygame import Rect, Surface
+from pygame import K_LEFT, K_RIGHT, K_UP, K_DOWN
 from pygame.event import Event
+from pygame.key import ScancodeWrapper
 from pygame.math import Vector2
 from pygame.sprite import Group, Sprite
 from pygame.time import Clock
@@ -28,8 +30,52 @@ class Player(Sprite):
         self.surface = Surface(size=size)
         self.surface.fill(color=fill_color)
         # The center kwarg... defines where it starts out?
-        self.rectangle: Rect = self.surface.get_rect(center=(10, HEIGHT - 30))
+        self.rectangle: Rect = self.surface.get_rect()
 
+        # Vector2 takes individual ordinates for its argument OR
+        # a tuple of coordinates.
+        self.position = Vector2((10, HEIGHT - 30))
+        self.velocity = Vector2(0, 0)
+        self.acceleration = Vector2(0, 0)
+    
+    def move(self) -> None:
+        self.acceleration = Vector2(0,0)
+
+        pressed_keys: ScancodeWrapper = pygame.key.get_pressed()
+
+        # Here, we're using ScancodeWrapper, which is a very bizarre
+        # class that uses an indexing scheme to address specific keys
+        # that are pulled as constants from the pygame main module.
+        if pressed_keys[K_LEFT]:
+            self.acceleration.x = -ACCELERATION
+        if pressed_keys[K_RIGHT]:
+            self.acceleration.x = ACCELERATION
+        # Origin is in the top-left, so these are opposite what
+        # one might expect
+        if pressed_keys[K_UP]:
+            self.acceleration.y = -ACCELERATION
+        if pressed_keys[K_DOWN]:
+            self.acceleration.y = ACCELERATION
+        
+        # Motion equations. Yawn.
+        self.acceleration += self.velocity * FRICTION
+        self.velocity += self.acceleration
+        self.position += self.velocity + (0.5 * self.acceleration)
+
+        # Make sure we're not out of bounds.
+        # Here, we do 'pacman-style' warping, where we exit
+        # the screen on one side and enter back in on the other
+        if self.position.x > WIDTH:
+            self.position.x = 0
+        if self.position.x < 0:
+            self.position.x = WIDTH
+        
+        if self.position.y > HEIGHT:
+            self.position.y = 0
+        if self.position.y < 0:
+            self.position.y = HEIGHT
+
+        self.rectangle.midbottom = self.position
 
 class Platform(Sprite):
     PLATFORM_DEFAULT_FILL = (255, 0, 0)
@@ -38,7 +84,12 @@ class Platform(Sprite):
         super().__init__()
         self.surface = Surface(size=size)
         self.surface.fill(color=fill_color)
-        self.rectangle: Rect = self.surface.get_rect(center=(initial_postition))
+        self.rectangle: Rect = self.surface.get_rect()
+
+        self.position = Vector2(initial_postition)
+        self.velocity = Vector2(0, 0)
+        self.acceleration = Vector2(0, 0)
+    
 
 
 def main():
@@ -65,8 +116,12 @@ def main():
         
         entity: Sprite
         for entity in all_sprites:
+            import pdb
+            # pdb.set_trace()
             display_surface.blit(source=entity.surface, dest=entity.rectangle)
         
+        player_1.move()
+
         pygame.display.update()
         game_clock.tick(FPS)
 
